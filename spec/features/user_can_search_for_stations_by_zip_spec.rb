e9c5ec261b2231088641cdf299510789ca0a2601
agpiermarini <piermarini.andrew@gmail.com>
2018-05-16 10:50:40 -0600

    Remove story comments from test

--- spec/features/user_can_search_for_stations_by_zip_spec.rb
+++ spec/features/user_can_search_for_stations_by_zip_spec.rb
@@ -4,13 +4,13 @@ feature 'User can visit root path' do
   context 'and search for fuel stations by zip code' do
     scenario 'and be directed to a page where they see the fuel stations at that zip code' do
       VCR.use_cassette("station-search-endpoint1") do
-      #as a user, visit root path
+
       visit "/"
-      # And I fill in the search form with 80203 (Note: Use the existing search form)
+
       fill_in :q, with: "80203"
-      # And I click "Locate"
+
       click_on "Locate"
-      # Then I should be on page "/search"
+
       expect(current_path).to eq(search_path)
       end
     end
@@ -21,9 +21,6 @@ feature 'User can visit root path' do
         fill_in :q, with: "80203"
         click_on "Locate"
 
-        # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-        # And the stations should be limited to Electric and Propane
-        # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
         expect(page).to have_css(".name1")
         expect(page).to have_css(".name10")
 
