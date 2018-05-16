2800d910fc2a940ed778facf550a1737da74538a
agpiermarini <piermarini.andrew@gmail.com>
2018-05-16 10:27:46 -0600

    Refactor view. Refactor StationSearch

--- app/services/station_search.rb
+++ app/services/station_search.rb
@@ -5,12 +5,6 @@ class StationSearch
   end
 
   def stations
-    conn = Faraday.new "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&radius=6&fuel_type=ELEC,LPG&limit=10"
-
-    response = conn.get do |req|
-      req.headers["X-API-KEY"] = api_key
-    end
-
     station_info = JSON.parse(response.body, symbolize_names: true)
 
     station_info[:fuel_stations].map do | station_hash |
@@ -20,5 +14,13 @@ class StationSearch
 
   private
     attr_reader :zip, :api_key
+    def connection
+      Faraday.new "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zip}&radius=6&fuel_type=ELEC,LPG&limit=10"
+    end
 
+    def response
+      connection.get do |req|
+        req.headers["X-API-KEY"] = api_key
+      end
+    end
 end
